aa44fda92b075e7c4f23c2fe061abedf94722198
BohdanShkil <bohdanshkill@gmail.com>
2026-09-24 23:27:22 +0200

    Add tests for byte order conversion and protocol string representation

--- tests/test_core_types.cpp
+++ tests/test_core_types.cpp
@@ -35,3 +35,25 @@ TEST(RawPacketTest, MoveSemantics){
     }
     EXPECT_TRUE(packet.data.empty());
 }
+
+TEST(ByteOrderTest, NetToHost16_HostToNet16){
+    std::uint16_t net_val = 0x1234;
+    std::uint16_t expected_host_val = 0x3412;
+    EXPECT_EQ(net_to_host_16(net_val), expected_host_val);
+    EXPECT_EQ(host_to_net_16(expected_host_val), net_val);
+}
+
+TEST(ByteOrderTest, NetToHost32_HostToNet32){
+    std::uint32_t net_val = 0x12345678;
+    std::uint32_t expected_host_val = 0x78563412;
+    EXPECT_EQ(net_to_host_32(net_val), expected_host_val);
+    EXPECT_EQ(host_to_net_32(expected_host_val), net_val);
+}
+
+TEST(ProtocolToStringTest, AllProtocols){
+    EXPECT_EQ(protocol_to_string(ProtocolType::TCP), "TCP");
+    EXPECT_EQ(protocol_to_string(ProtocolType::UDP), "UDP");
+    EXPECT_EQ(protocol_to_string(ProtocolType::ICMP), "ICMP");
+    EXPECT_EQ(protocol_to_string(ProtocolType::ICMPv6), "ICMPv6");
+    EXPECT_EQ(protocol_to_string(ProtocolType::Unknown), "Unknown");
+}
